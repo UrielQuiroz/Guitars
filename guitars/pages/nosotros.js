a3d300e223921a71b84b146850a6a966eca9c905
UrielQuiroz <uriel.quiroz187@gmail.com>
2022-01-06 13:46:17 -0600

    Routing en Next js

---
+++ guitars/pages/nosotros.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Link from 'next/link';
+
+const Nosotros = () => {
+    return (
+        <div>
+            <h1>Desde Nosotros</h1>
+            <Link href='/' >Ir a Inicio</Link>
+        </div>
+    )
+}
+
+export default Nosotros
