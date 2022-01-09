e12aca35d5498b80f3387113ca999949e5d86176
UrielQuiroz <uriel.quiroz187@gmail.com>
2022-01-08 21:39:50 -0600

    Sección Nosotros
    Se creo el contenido que se mostrara la parte de "Nosotros"

--- guitars/pages/nosotros.js
+++ guitars/pages/nosotros.js
@@ -1,17 +1,45 @@
-import React from 'react';
-import Link from 'next/link';
-import Layout from '../components/Layout';
+import React from "react";
+import Image from 'next/image';
+import Layout from "../components/Layout";
+import styles from '../styles/Nosotros.module.css';
 
 const Nosotros = () => {
-    return (
-        <Layout
-            pagina="Nosotros">
+  return (
+    <Layout pagina="Nosotros">
+      <main className="contenedor">
+        <h2 className="heading">Nosotros</h2>
 
-                <h1>Desde Nosotros</h1>
-                <Link href='/' >Ir a Inicio</Link>
-                
-        </Layout>
-    )
-}
+            <div className={styles.contenido}>
 
-export default Nosotros
+                <Image layout="responsive" width={600} height={450} src='/img/nosotros.jpg' alt="Imagen sobre nosotros" />
+
+                <div>
+                    <p>Nunc lacinia nibh nec tortor vehicula vulputate. In hac habitasse
+                        platea dictumst. Phasellus eget bibendum mauris. Donec euismod
+                        condimentum nisl, et mattis mauris. Duis finibus turpis leo, vitae
+                        mollis nunc accumsan sit amet. Aliquam eu risus odio. Nam eu
+                        suscipit ex, eu cursus nunc. Ut congue dictum turpis ut rutrum. Ut
+                        in euismod dui. Praesent iaculis fermentum tristique. Aliquam
+                        pharetra urna risus, sodales facilisis magna condimentum non. Etiam
+                        euismod scelerisque sem molestie euismod.</p>
+
+                    <p>Nunc lacinia nibh nec tortor vehicula vulputate. In hac habitasse
+                        platea dictumst. Phasellus eget bibendum mauris. Donec euismod
+                        condimentum nisl, et mattis mauris. Duis finibus turpis leo, vitae
+                        mollis nunc accumsan sit amet. Aliquam eu risus odio. Nam eu
+                        suscipit ex, eu cursus nunc. Ut congue dictum turpis ut rutrum. Ut
+                        in euismod dui. Praesent iaculis fermentum tristique. Aliquam
+                        pharetra urna risus, sodales facilisis magna condimentum non. Etiam
+                        euismod scelerisque sem molestie euismod. Duis ullamcorper, tellus
+                        ac gravida malesuada, risus nulla iaculis augue, sit amet auctor est
+                        augue a ex.</p>
+                </div>
+
+            </div>
+
+      </main>
+    </Layout>
+  );
+};
+
+export default Nosotros;
